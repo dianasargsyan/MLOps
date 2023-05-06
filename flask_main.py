873f7ffe28f4b8f2332397cf4303b3d7ea875726
dianasargsyan <diana_sargsyan2@edu.aua.am>
2023-05-06 18:37:48 +0400

    adding monitoring system

--- flask_main.py
+++ flask_main.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
@@ -6,17 +7,36 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from model.utils import check_missing_values, clean_car_names, convert_symboling_to_string, select_features
+from prometheus_client import Counter, Gauge, Histogram, Summary
+
+
 
 checkpoint_file = './checkpoints/model_checkpoint2.joblib'
 model = joblib.load(checkpoint_file)
 
 
+REQUEST_COUNTER = Counter("request_count", "The number of requests")
+REQUEST_TIME = Summary("request_processing_seconds", "Time spent processing request")
+PREDICTED_PRICE = Gauge("predicted_price", "The predicted price for a car")
+
+
 def home():
     return "Use this API to predict car prices."
+    
+def summary():
+    return ""
+
+
+@REQUEST_TIME.time()
+ 
+
+    
 
 
 def predict_price(wheelbase, carlength, carwidth, carheight, curbweight, enginesize, 
                   boreratio, stroke, compressionratio, horsepower, peakrpm, citympg, highwaympg):
+    
+    REQUEST_COUNTER.inc()
 
     # Load the feature ranges
     input_df = pd.DataFrame({
@@ -35,6 +55,11 @@ def predict_price(wheelbase, carlength, carwidth, carheight, curbweight, engines
         'highwaympg': [highwaympg]
     })
     
+
+
     # Make a prediction using the loaded model
-    prediction = model.predict(input_df)
-    return jsonify({'predicted_price': prediction[0]})
+    prediction = model.predict(input_df)[0]
+    PREDICTED_PRICE.set(prediction)
+    return jsonify({'predicted_price': prediction})    
+
+    
